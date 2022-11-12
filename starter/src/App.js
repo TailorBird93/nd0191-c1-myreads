850deab157a8eb52a50a6cd885105e296be7599c
TailorBird93 <99030950+TailorBird93@users.noreply.github.com>
2022-11-12 18:19:13 +0000

    Added search function and fixed error with handleChange function not being read in the search page

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
+import * as BooksAPI from "./BooksAPI";
 import { useState, useEffect } from "react";
 
-import {getAll, update} from "./BooksAPI";
-
-import SearchPage from "./components/SearchPage";
+import {getAll, update, search} from "./BooksAPI";
+import Book from "./components/Book";
 import Header from "./components/Header";
 import Shelves from "./components/Shelves";
 import OpenSearch from "./components/OpenSearch";
@@ -12,6 +12,20 @@ function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
 
   const [books, setBooks] = useState([]);
+
+
+  const [findBooks, setFindBooks] = useState([]);
+
+  const lookForBook= async (e) => {
+    const response = await search(e.target.value, 0);
+      if (Array.isArray(response)) {
+        setFindBooks(response);
+        document.getElementById("noBooks").innerHTML="Matching Titles:"
+      } else {
+        setFindBooks([]);
+        document.getElementById("noBooks").innerHTML="No Matching Titles. Please enter a valid name"
+      }
+  } 
 
 
 
@@ -31,12 +45,34 @@ function App() {
   return (
     <div className="app">
       {showSearchPage ? (
-        <SearchPage  showSearchPage={showSearchPage} setShowSearchpage={setShowSearchpage}/>
+        <div className="search-books">
+        <div className="search-books-bar">
+          <a
+            className="close-search"
+            onClick={() => setShowSearchpage(!showSearchPage)}
+          >
+            Close
+          </a>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title, author, or ISBN"
+              onChange={lookForBook}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <h2 id="noBooks"></h2>
+          <ol className="books-grid">
+            {findBooks.map((book) => <Book book={book} key={book.id} handleChange = {handleChange} />)}
+          </ol>
+        </div>
+      </div>
       ) : (
         <div className="list-books">
           <Header />
           <Shelves books={books} setBooks={setBooks} handleChange={handleChange}/>
-          <OpenSearch showSearchPage={showSearchPage} setShowSearchpage={setShowSearchpage}/>
+          <OpenSearch showSearchPage={showSearchPage} setShowSearchpage={setShowSearchpage} handleChange={handleChange}/>
         </div>
       )}
     </div>
