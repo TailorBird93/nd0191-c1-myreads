155fc69da543d170ac33b704ab769849446ae4c0
TailorBird93 <99030950+TailorBird93@users.noreply.github.com>
2022-11-13 11:30:23 +0000

    Fixed issue with displaying multiple authors per book, edited the README.md file

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -32,6 +32,7 @@ const Book = ({book, handleChange}) => {
             </div>
             <div className="book-title">{book.title}</div>
             <div className="book-authors">{book.authors}</div>
+            
         </div>
     )
 }
